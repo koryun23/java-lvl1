94ee35c9f0eb626adc3b7fb992a2fa715ba365e5
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-01-07 15:42:50 +0400

    committing the exercise 3

--- src/main/java/homework2/Ex3.java
+++ src/main/java/homework2/Ex3.java
@@ -6,8 +6,14 @@ public class Ex3 {
     }
     public static void quadraticEq(int a, int b, int c){
         double squareRootD = Math.sqrt(b*b-4*a*c);
-        double x1 = (-b+squareRootD)/(2*a);
-        double x2 = (-b-squareRootD)/(2*a);
-        System.out.println("The roots are "+ x1 +" and "+ x2);
+        if(squareRootD < 0){
+            System.out.println("No solutions");
+        }else if(squareRootD == 0){
+            System.out.println("The root is "+ (-b/(2*a)));
+        }else {
+            double x1 = (-b + squareRootD) / (2 * a);
+            double x2 = (-b - squareRootD) / (2 * a);
+            System.out.println("The roots are " + x1 + " and " + x2);
+        }
     }
 }
