cdd1cbe2260dba4aa757030b6c9e40e41ddb685c
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-01-15 15:09:31 +0400

    committing exercise 2 of homework3

---
+++ src/main/java/homework3/Ex2.java
@@ -0,0 +1,24 @@
+package homework3;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        boolean countIs10 = count10();
+        String output = countIs10 ? "Yes" : "No";
+        System.out.println(output);
+    }
+
+    public static boolean count10(){
+        System.out.println("Enter a number");
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0; // counts the numbers equal to 10
+        for(int i = 0; i < 6; i++){
+            int num = scanner.nextInt();
+            if (num == 10){
+                counter++;
+            }
+        }
+        return counter==1;
+    }
+}
