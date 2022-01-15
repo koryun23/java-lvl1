887cb32423323c5a53a0941762599f86f038d3db
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-01-15 16:31:07 +0400

    committing exercise 7 of homework3

--- src/main/java/homework3/Ex7.java
+++ src/main/java/homework3/Ex7.java
@@ -2,26 +2,22 @@ package homework3;
 
 import java.util.Scanner;
 
-public class Ex6 {
+public class Ex7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        System.out.println(fibonacci(n));
+        System.out.println(fibonacciSum(n));
     }
-
-    public static int fibonacci(int n){
+    public static int fibonacciSum(int n){
+        int sum = 0;
         int prev = 1;
         int current = 0;
         for (int i = 0; i < n; i++){
             int next = current+prev;
             prev = current;
             current = next;
+            sum+=current;
         }
-        return current;
-    }
-
-    public static int fibonacciRecursive(int n){
-        if(n <= 2) return 1;
-        return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+        return sum;
     }
 }
