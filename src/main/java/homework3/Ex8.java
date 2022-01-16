be1751acc23511f82e7a73ea1dc093d5f7b1049a
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-01-17 01:20:56 +0400

    making some final improvements to the ship battle game

--- src/main/java/homework3/Ex8.java
+++ src/main/java/homework3/Ex8.java
@@ -32,85 +32,163 @@ public class Ex8 {
         board[9-y][x] = 'X';
         return false;
     }
-    public static void placeShips(char[][] board) {
-        int block1 = 4;
-        int block2 = 3;
-        int block3 = 2;
-        int block4 = 1;
-        while (block1 > 0 || block2 > 0 || block3 > 0 || block4 > 0) {
-            System.out.println("How many blocks do you want your ship to have(1, 2, 3, 4)");
-            Scanner scanner = new Scanner(System.in);
-            int blocks = scanner.nextInt();
-            int startingX = -1, startingY = -1, endingX = -1, endingY = -1;
-            if (blocks == 1) {
-                if (block1 > 0) {
-                    System.out.println("Write the x coordinate for your ship: [0-9]");
-                    startingX = scanner.nextInt();
-                    System.out.println("Write the y coordinate for your ship: [0-9]");
-                    startingY = scanner.nextInt();
-                    if (startingX < 0 || startingX > 9 || startingY < 0 || startingY > 9){
-                        System.out.println("Invalid coordinates.");
-                    }else {
-                        board[9 - startingY][startingX] = '#'; // place a ship there
-                        showBoard(board);
-                        block1--;
-                    }
-                }else{
-                    System.out.println("Can't place the ship.");
-                }
-            } else {
-                int block = 0;
-                if (blocks == 2) {
-                    block = block2;
-                }else if(blocks == 3){
-                    block = block3;
-                }else if(blocks == 4){
-                    block = block4;
-                }
-                if(block > 0) {
-                    System.out.println("Write the x coordinate of the first block of your ship: [0-9]");
-                    startingX = scanner.nextInt();
-                    System.out.println("Write the y coordinate of the first block of your ship: [0-9]");
-                    startingY = scanner.nextInt();
-
-                    System.out.println("Write the x coordinate of the last block of your ship: [0-9]");
-                    endingX = scanner.nextInt();
-                    System.out.println("Write the y coordinate of the last block of your ship: [0-9]");
-                    endingY = scanner.nextInt();
-
-                    if (startingX == endingX){
-                        if (Math.abs(startingY-endingY) >= blocks){
-                            System.out.println("Invalid coordinates.");
-                        }else {
-                            for (int i = startingY; i <= endingY; i++) {
-                                board[9 - i][startingX] = '#';
-                            }
-                            if (blocks == 2) block2--;
-                            else if(blocks==3) block3--;
-                            else if(blocks==6) block4--;
-                            showBoard(board);
-                        }
-                    }else if(startingY == endingY){
-                        if (Math.abs(startingX-endingX) >= blocks){
-                            System.out.println("Invalid coordinates.");
-                        }else {
-                            for (int i = startingX; i <= endingX; i++) {
-                                board[9 - startingY][i] = '#';
-                            }
-                            if (blocks == 2) block2--;
-                            else if(blocks==3) block3--;
-                            else if(blocks==4) block4--;
-                            showBoard(board);
-                        }
-                    }else{
-                        System.out.println("Invalid coordinates.");
-                    }
-
-                }else{
-                    System.out.println("Can't place the ship.");
+    public static int numOfAdjacentBlocks(char[][] board, int i, int j){
+        System.out.println(board[9][0]);
+        int[][] directions = {
+                {i-1,j-1},
+                {i-1, j},
+                {i-1, j+1},
+                {i, j-1},
+                {i, j+1},
+                {i+1, j-1},
+                {i+1, j},
+                {i+1, j+1}
+        };
+        int counter = 0;
+        for(int[] dir : directions){
+            if (dir[0] >= 0 && dir[0] < 10 && dir[1] >= 0 && dir[1] < 10){
+                // then the cell is inside the board
+                // we check if there is a block of ship in that cell
+                char currentCell = board[9-dir[1]][dir[0]];
+                if(currentCell == '#'){
+                    counter++;
                 }
             }
+        }
+        return counter;
+    }
 
+    public static void placeShips(char[][] board) {
+        int block1 = 3;
+        int block2 = 2;
+        int block3 = 2;
+        int block4 = 1;
+        int startingX = -1, startingY = -1, endingX = -1, endingY = -1, temp=0;
+        while (block1 > 0 || block2 > 0 || block3 > 0 || block4 > 0) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter * to see the board, --insert to insert a ship or --reset to reset a ship.");
+            String answer = scanner.nextLine();
+            if ("--insert".equals(answer)) {
+                System.out.println("How many blocks do you want your ship to have(1, 2, 3, 4)");
+                int blocks = scanner.nextInt();
+
+                if (blocks == 1) {
+                    if (block1 > 0) { // we check if there are less then 4 ships having only 1 block
+                        System.out.println("Write the x coordinate for your ship: [0-9]");
+                        startingX = scanner.nextInt();
+                        System.out.println("Write the y coordinate for your ship: [0-9]");
+                        startingY = scanner.nextInt();
+                        if (startingX < 0 || startingX > 9 || startingY < 0 || startingY > 9) {
+                            System.out.println("Invalid coordinates.");
+                        } else if (numOfAdjacentBlocks(board, startingX, startingY) > 0) {
+                            System.out.println("Can't place the ship. There is a ship adjacent to the entered coordinates.");
+                        } else {
+                            board[9 - startingY][startingX] = '#'; // place a ship there
+                            showBoard(board);
+                            block1--;
+                        }
+                    } else {
+                        System.out.println("Can't place the ship.");
+                    }
+                } else {
+                    int block = 0;
+                    if (blocks == 2) {
+                        block = block2;
+                    } else if (blocks == 3) {
+                        block = block3;
+                    } else if (blocks == 4) {
+                        block = block4;
+                    }
+                    if (block > 0) {
+                        System.out.println("Write the x coordinate of the first block of your ship: [0-9]");
+                        startingX = scanner.nextInt();
+                        System.out.println("Write the y coordinate of the first block of your ship: [0-9]");
+                        startingY = scanner.nextInt();
+
+                        System.out.println("Write the x coordinate of the last block of your ship: [0-9]");
+                        endingX = scanner.nextInt();
+                        System.out.println("Write the y coordinate of the last block of your ship: [0-9]");
+                        endingY = scanner.nextInt();
+                        boolean hasAdjacentBlocks = false;
+                        temp = 0;
+                        if (startingY > endingY) { // swap 'em
+                            temp = startingY;
+                            startingY = endingY;
+                            endingY = temp;
+                        }
+                        if (startingX > endingX) {  // swap 'em
+                            temp = startingX;
+                            startingX = endingX;
+                            endingX = temp;
+                        }
+                        System.out.println("Start x: " + startingX);
+                        System.out.println("End x: " + endingX);
+                        System.out.println("Start y: " + startingY);
+                        System.out.println("End y: " + endingY);
+                        if (startingX == endingX) {
+                            if (Math.abs(startingY - endingY) != blocks - 1) {
+                                System.out.println("Invalid coordinates.");
+                            } else {
+
+                                hasAdjacentBlocks = false;
+                                for (int i = startingY; i <= endingY; i++) {
+//                                board[9 - i][startingX] = '#';
+                                    if (numOfAdjacentBlocks(board, startingX, i)>0) {
+                                        hasAdjacentBlocks = true;
+                                        break;
+                                    }
+                                }
+                                if (!hasAdjacentBlocks) {
+                                    for (int i = startingY; i <= endingY; i++) {
+                                        board[9 - i][startingX] = '#';
+                                    }
+                                    if (blocks == 2) block2--;
+                                    else if (blocks == 3) block3--;
+                                    else if (blocks == 4) block4--;
+                                    showBoard(board);
+                                } else {
+                                    System.out.println("Can't place the ship. There is a ship adjacent to the entered coordinates.");
+                                }
+
+                            }
+                        } else if (startingY == endingY) {
+                            if (Math.abs(startingX - endingX) != blocks - 1) {
+                                System.out.println("Invalid coordinates.");
+                            } else {
+
+                                hasAdjacentBlocks = false;
+                                for (int i = startingX; i <= endingX; i++) {
+                                    if (numOfAdjacentBlocks(board, i, startingY)>0) {
+                                        hasAdjacentBlocks = true;
+                                        break;
+                                    }
+                                }
+                                if (!hasAdjacentBlocks) {
+                                    for (int i = startingX; i <= endingX; i++) {
+                                        board[9 - startingY][i] = '#';
+                                    }
+                                    if (blocks == 2) block2--;
+                                    else if (blocks == 3) block3--;
+                                    else if (blocks == 4) block4--;
+                                    showBoard(board);
+                                } else {
+                                    System.out.println("Can't place the ship. There is a ship adjacent to the entered coordinates.");
+                                }
+                            }
+                        } else {
+                            System.out.println("Invalid coordinates.");
+                        }
+
+                    } else {
+                        System.out.println("Can't place the ship.");
+                    }
+                }
+            }else if("*".equals(answer)){
+                showBoard(board);
+            }
+            else {
+                System.out.println("Could not recognize the command");
+            }
         }
 
     }
