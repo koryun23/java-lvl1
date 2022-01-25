5c78b071131625f64948b5bf31071243c75f6a6b
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-01-25 18:10:51 +0400

    solution for the quadratic equation problem

---
+++ src/com/aca/homework/week1/quadraticeequation/QuadraticEquation.java
@@ -0,0 +1,19 @@
+package com.aca.homework.week1.quadraticeequation;
+
+public class QuadraticEquation {
+    public static void main(String[] args) {
+        quadraticEq(1, 5, 1);
+    }
+    public static void quadraticEq(int a, int b, int c){
+        double squareRootD = Math.sqrt(b*b-4*a*c);
+        if(squareRootD < 0){
+            System.out.println("No solutions");
+        }else if(squareRootD == 0){
+            System.out.println("The root is "+ (-b/(2*a)));
+        }else {
+            double x1 = (-b + squareRootD) / (2 * a);
+            double x2 = (-b - squareRootD) / (2 * a);
+            System.out.println("The roots are " + x1 + " and " + x2);
+        }
+    }
+}
