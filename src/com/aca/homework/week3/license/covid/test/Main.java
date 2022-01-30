47471f674bb73f1c6999808a58c20da84d3b9bed
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-01-30 22:46:17 +0400

    Adding the requirements of the license-covid-test problem.

--- src/com/aca/homework/week3/license/covid/test/Main.java
+++ src/com/aca/homework/week3/license/covid/test/Main.java
@@ -1,6 +1,16 @@
 package com.aca.homework.week3.license.covid.test;
 
 public class Main {
+/*
+    Create a User having fields username and name. Create a CovidTest class that has fields user and result. The result can be one of the following values: "undefined", "positive", "negative".
+    By default, the covid test result should be "undefined".
+    Make all User's and CovidTest's fields private.
+    Create a test() method in CovidTest class which in 3 seconds randomly set any test result.
+    Create a DrivingLicense class that has fields user and an array of granted license categories (may contain 'A', 'B, 'C', 'D', 'E')
+    There is a person having "vova_gasparyan" username and "Vova" name who gave a test and waiting to see his covid test result.
+    Before giving a test the policeman stopped Vova who had to show his driving license of BC type.
+    note: Only User, CovidTest, DrivingLicense, and Main classes should be created.
+*/
     public static void main(String[] args) {
         User user = new User("Vova", "vova_gasparyan");
         DrivingLicense license = new DrivingLicense(user);
