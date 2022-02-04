59aa0c9b5cb0410a0eb340ab12328dc04b9da195
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-02-04 16:21:30 +0400

    The solution of color pixel problem

--- src/com/aca/homework/week4/color/pixel/ColorPixel.java
+++ src/com/aca/homework/week4/color/pixel/ColorPixel.java
@@ -1,13 +1,9 @@
 package com.aca.homework.week4.color.pixel;
 
 public class ColorPixel extends Pixel{
-    private int x;
-    private int y;
     private String color;
     public ColorPixel(int x, int y, String color){
         super(x, y);
-        this.x = x;
-        this.y = y;
         this.color = color;
     }
 
