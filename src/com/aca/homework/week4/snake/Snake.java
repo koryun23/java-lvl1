e0243e561894b1c9d22e30d0ac6cad4a774a9406
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-02-08 21:18:46 +0400

    pushing the improved version of the snake game

--- src/com/aca/homework/week4/snake/Snake.java
+++ src/com/aca/homework/week4/snake/Snake.java
@@ -4,28 +4,40 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Snake {
-    private char[][] board;
+    private char[][] board = new char[8][8];
     private int[][] snake;
     private int snakeSize = 1;
-    private int appleRow;
-    private int appleCol;
+    private int appleRow = 2;
+    private int appleCol = 1;
+    private int headRow = 0;
+    private int headCol = 0;
     private boolean running = true; // if the player loses, the variable becomes false
 
-    public Snake(char[][] board, int headRow, int headCol, int appleRow, int appleCol) {
-        this.board = board;
-        this.snake = new int[board.length*board[0].length][2];
-        this.snake[snakeSize-1][0] = headRow;
-        this.snake[snakeSize-1][1] = headCol;
+    public Snake() {
+        initializeBoard();
+        initializeSnake();
         this.board[headRow][headCol] = '*';
-        this.appleRow = appleRow;
-        this.appleCol = appleCol;
         this.board[appleRow][appleCol] = '=';
     }
+
     public String getMove(){
         Scanner scanner = new Scanner(System.in);
         String move = scanner.nextLine();
         return move;
     }
+
+    public boolean snakeCannotMove(int dirX, int dirY){
+        return this.snakeSize > 1 && this.snake[0][0]+dirY == this.snake[1][0] && this.snake[0][1]+dirX == this.snake[1][1];
+    }
+
+    public boolean snakeAteAppleOnPrevMove(int dirX, int dirY){
+        return snake[0][0]+dirY == this.appleRow && this.snake[0][1]+dirX == this.appleCol;
+    }
+
+    public boolean snakeEatsAppleOnNextMove(int dirX, int dirY){
+        return this.snake[0][0] == this.appleRow && this.snake[0][1] == this.appleCol;
+    }
+
     public void move(){
         String move = this.getMove();
         int dirX = 0, dirY = 0;
@@ -40,15 +52,16 @@ public class Snake {
         }
 
 
-        if(this.snakeSize > 1 && this.snake[0][0]+dirY == this.snake[1][0] && this.snake[0][1]+dirX == this.snake[1][1]){
+        if(snakeCannotMove(dirX, dirY)){
             return;
         }
-        if(this.isOutOfBounds(snake[0][0]+dirY, snake[0][1]+dirX)){
+        if(isOutOfBounds(snake[0][0]+dirY, snake[0][1]+dirX)){ // checking if the snake will leave the board on the next turn
             System.out.println("You lose");
             this.running = false;
             return;
         }
-        if(snake[0][0]+dirY == this.appleRow && this.snake[0][1]+dirX == this.appleCol){
+
+        if(snakeAteAppleOnPrevMove(dirX, dirY)){
             this.updateSnake(dirY, dirX);
             this.updateBoard();
             this.generateApple();
@@ -56,10 +69,9 @@ public class Snake {
             this.snakeSize++;
         }
 
-        else if (this.snake[0][0] == this.appleRow && this.snake[0][1] == this.appleCol){ // generate a new apple
+        else if (snakeEatsAppleOnNextMove(dirX, dirY)){
             int[] snakeHead = this.snake[0];
             this.board[snakeHead[0]+dirY][snakeHead[1]+dirX] = '*';
-
             this.updateSnake(dirY, dirX);
             this.updateBoard();
             this.show();
@@ -68,13 +80,13 @@ public class Snake {
             this.updateSnake(dirY, dirX);
             this.updateBoard();
             this.show();
-            // check if after moving the snake bit itself
             if(this.snakeBitItself()){
                 System.out.println("You lose");
                 this.running = false;
             }
         }
     }
+
     public void generateApple(){
         System.out.println("Generating apples");
         int newAppleRow = -1, newAppleCol = -1;
@@ -88,6 +100,7 @@ public class Snake {
         this.appleCol = newAppleCol;
 
     }
+
     public void updateSnake(int dirRow, int dirCol){
         for(int i = snakeSize; i>0; i--){
             snake[i][0] = snake[i-1][0];
@@ -99,7 +112,7 @@ public class Snake {
     }
 
     public void updateBoard(){
-        this.fillWithZeroes();
+        this.initializeBoard();
         this.board[appleRow][appleCol] = '=';
         for(int i = 0; i < snakeSize; i++){
             this.board[snake[i][0]][snake[i][1]] = '*';
@@ -115,20 +128,28 @@ public class Snake {
         }
     }
 
-    public void fillWithZeroes() {
+    public void initializeBoard() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                this.board[i][j] = '0';
+                board[i][j] = '0';
             }
         }
+    }
+
+    public void initializeSnake(){
+        this.snake = new int[board.length*board[0].length][2]; // snake array represents the cells occupied by the snake
+        this.snake[snakeSize-1][0] = this.headRow;
+        this.snake[snakeSize-1][1] = this.headCol;
     }
 
     public boolean cellIsEmpty(int row, int col){
         return !isOutOfBounds(row, col) && this.board[row][col] == '0';
     }
+
     public boolean isOutOfBounds(int row, int col){
         return row < 0 || row >= this.board.length || col < 0 || col >= this.board[0].length;
     }
+
     public boolean snakeBitItself(){
         for(int i = 3; i < this.snakeSize; i++){
             if (this.snake[0][0] == this.snake[i][0] && this.snake[0][1] == this.snake[i][1]){
--- src/com/aca/homework/week4/snake/SnakeDemo.java
+++ src/com/aca/homework/week4/snake/SnakeDemo.java
@@ -2,19 +2,10 @@ package com.aca.homework.week4.snake;
 
 public class SnakeDemo {
     public static void main(String[] args) {
-        char[][] board = new char[8][8];
-        fillWithZeroes(board);
-        Snake snake = new Snake(board, 0, 0, 2, 1);
+        Snake snake = new Snake();
         snake.show();
         while(snake.isGameRunning()){
             snake.move();
-        }
-    }
-    public static void fillWithZeroes(char[][] board) {
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                board[i][j] = '0';
-            }
         }
     }
 }
