48144f33c68078d41545e4ae908c3fe4bfe3850f
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-02-13 00:51:13 +0400

    The solution of the snake game

--- src/com/aca/homework/week4/snake/SnakeDemo.java
+++ src/com/aca/homework/week4/snake/SnakeDemo.java
@@ -2,10 +2,10 @@ package com.aca.homework.week4.snake;
 
 public class SnakeDemo {
     public static void main(String[] args) {
-        Snake snake = new Snake();
-        snake.show();
-        while(snake.isGameRunning()){
-            snake.move();
+        SnakeGame snakeGame = new SnakeGame();
+        snakeGame.show();
+        while (snakeGame.isGameRunning()) {
+            snakeGame.play();
         }
     }
 }
