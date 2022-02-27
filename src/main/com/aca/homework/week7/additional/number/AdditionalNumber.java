f514d590f5c4ce75117eddc3ccb39341753cde32
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-02-27 12:23:13 +0400

    solution of the additional number problem using bit manipulation

--- src/main/com/aca/homework/week7/additional/number/AdditionalNumber.java
+++ src/main/com/aca/homework/week7/additional/number/AdditionalNumber.java
@@ -1,39 +1,30 @@
 package com.aca.homework.week7.additional.number;
 
 public class AdditionalNumber {
-    public static int max(int[] arr) {
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) max = arr[i];
-        }
-
-        return max;
-    }
-
     public static int getAdditionalNumber(int[] firstArray, int[] secondArray) {
-        int[] temp  = new int[firstArray.length * 2 - 1];
-        for(int i = 0; i < firstArray.length; i++){
-            if(temp[firstArray.length-1 + firstArray[i] % firstArray.length] == 0){
-                temp[firstArray.length-1 + firstArray[i] % firstArray.length] = 1;
-            }else{
-                temp[firstArray.length-1 + firstArray[i] % firstArray.length]++;
-            }
-        }
-
-        for(int i = 0; i < secondArray.length; i++){
-            temp[firstArray.length-1+secondArray[i]%firstArray.length]--;
-        }
-
-        for(int i = 0; i < temp.length; i++){
-            if(temp[i] == 1){
-                return firstArray.length;
-            }
-        }
         return -1;
     }
 
+
     public static void main(String[] args) {
-        int[] arr1 = {3, 1, 4, 2, 5}, arr2 = {2, 4, 1, 3};
-        System.out.println(getAdditionalNumber(arr1, arr2));
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 6, 7}, arr2 = {1, 2, 3, 4, 5, 6, 6};
+        long startTime = System.currentTimeMillis();
+        System.out.println(xorSolution(arr1, arr2));
+        System.out.println((double) System.currentTimeMillis() - startTime);
+    }
+
+
+    public static int xorSolution(int[] firstArray, int[] secondArray){
+        int xorOfFirstArray = getArrayXor(firstArray);
+        int xorOfSecondArray = getArrayXor(secondArray);
+        return xorOfFirstArray ^ xorOfSecondArray;
+    }
+
+    public static int getArrayXor(int[] arr){
+        int xor = 0;
+        for(int num : arr){
+            xor ^= num;
+        }
+        return xor;
     }
 }
