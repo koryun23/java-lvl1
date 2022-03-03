c5910d7433129491aed44b17b49f1cc4b2f4e3af
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-03 16:55:59 +0400

    The solution of the 'checked unchecked exceptions' problem

---
+++ src/test/com/aca/homework/week8/checked/unchecked/exceptions/MainTest.java
@@ -0,0 +1,58 @@
+package com.aca.homework.week8.checked.unchecked.exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void testMethod1() {
+        assertThrows(RuntimeException.class, new Executable() {
+            @Override
+            public void execute() {
+                Main.method1();
+            }
+        });
+    }
+
+    @Test
+    public void testMethod2() {
+        assertThrows(RuntimeException.class, new Executable() {
+            @Override
+            public void execute() {
+                Main.method2();
+            }
+        });
+    }
+
+    @Test
+    public void testMethod3() {
+        assertThrows(Exception.class, new Executable() {
+            @Override
+            public void execute() {
+                Main.method3();
+            }
+        });
+    }
+
+    @Test
+    public void testMethod4() {
+        assertThrows(Exception.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                Main.method4();
+            }
+        });
+    }
+
+    @Test
+    public void testMethod5() {
+        assertThrows(Exception.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                Main.method5();
+            }
+        });
+    }
+}
