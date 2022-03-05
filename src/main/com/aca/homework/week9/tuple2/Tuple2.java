2fb7dc2e869bd78d8c8a21e6594a1212475df623
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-05 12:29:05 +0400

    The solution of the 'tuple2' problem

--- src/main/com/aca/homework/week9/tuple2/Tuple2.java
+++ src/main/com/aca/homework/week9/tuple2/Tuple2.java
@@ -1,10 +1,10 @@
 package com.aca.homework.week9.tuple2;
 
 public class Tuple2<T1, T2> {
-    private T1 t1;
-    private T2 t2;
+    private final T1 t1;
+    private final T2 t2;
 
-    public Tuple2(T1 t1, T2 t2){
+    public Tuple2(T1 t1, T2 t2) {
         this.t1 = t1;
         this.t2 = t2;
     }
--- src/test/com/aca/homework/week9/tuple2/Tuple2Test.java
+++ src/test/com/aca/homework/week9/tuple2/Tuple2Test.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Test;
 
 class Tuple2Test {
     @Test
-    public void testTuple2ForStringAndInteger(){
+    public void testTuple2ForStringAndInteger() {
         Tuple2<String, Integer> tuple2 = new Tuple2<>("text", 45);
         Assertions.assertEquals("text", tuple2.getT1());
         Assertions.assertEquals(45, tuple2.getT2());
     }
 
     @Test
-    public void testTuple2ForLongAndInteger(){
+    public void testTuple2ForLongAndInteger() {
         Tuple2<Long, Integer> tuple2 = new Tuple2<>(100L, 45);
         Assertions.assertEquals(100, tuple2.getT1());
         Assertions.assertEquals(45, tuple2.getT2());
