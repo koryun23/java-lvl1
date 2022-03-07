a6d0198220e40cf60922d88a62ac0790aefc38c6
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-07 17:39:52 +0400

    changing the access modifier for the elements array from package-private to private

--- src/main/com/aca/homework/week9/generic/queue/Queue.java
+++ src/main/com/aca/homework/week9/generic/queue/Queue.java
@@ -1,7 +1,7 @@
 package com.aca.homework.week9.generic.queue;
 
 public class Queue<T> {
-    final Object[] elements = new Object[10_000];
+    private final Object[] elements = new Object[10_000];
     private int produceIndex = 0;
 
     public void produce(T object) {
