ec9a73722af5c690f4f83b3ebeb4482f31e4dac7
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-13 14:26:39 +0400

    the solution of the 'template' problem

---
+++ src/main/com/aca/homework/week10/template/Demo.java
@@ -0,0 +1,14 @@
+package com.aca.homework.week10.template;
+
+public class Demo {
+    public static void main(String[] args) {
+        PageRenderer pageRenderer = new PageRenderer();
+        pageRenderer.print();
+        System.out.println("---------------------------");
+        PageRenderer topPageNumberAwareRenderer = new TopPageNumberAwareRenderer();
+        topPageNumberAwareRenderer.print();
+        System.out.println("---------------------------");
+        PageRenderer topAndDownPageNumberAwarePageRenderer = new TopAndDownPageNumberAwarePageRenderer();
+        topAndDownPageNumberAwarePageRenderer.print();
+    }
+}
