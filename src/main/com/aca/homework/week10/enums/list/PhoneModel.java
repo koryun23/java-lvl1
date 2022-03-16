ab7be85302e20df553701919c9cc09579e72c46a
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-16 17:19:48 +0400

    changing the logic of getting the camera characteristics of phones

--- src/main/com/aca/homework/week10/enums/list/PhoneModel.java
+++ src/main/com/aca/homework/week10/enums/list/PhoneModel.java
@@ -1,26 +1,31 @@
 package com.aca.homework.week10.enums.list;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public enum PhoneModel {
     NOKIA_1100 {
+        private final List<String> cameras = Collections.emptyList();
+
         @Override
-        public List<String> cameras(){
-            return Collections.emptyList();
+        public List<String> cameras() {
+            return cameras;
         }
     },
     NOKIA_3110 {
+        private final List<String> cameras = List.of("back camera");
+
         @Override
-        public List<String> cameras(){
-            return new ArrayList<>(List.of("back camera"));
+        public List<String> cameras() {
+            return cameras;
         }
     },
     GALAXY_S2 {
+        private final List<String> cameras = List.of("back camera", "front camera");
+
         @Override
-        public List<String> cameras(){
-            return new ArrayList<>(List.of("back camera", "front camera"));
+        public List<String> cameras() {
+            return cameras;
         }
     };
 
--- src/test/com/aca/homework/week10/enums/list/PhoneModelTest.java
+++ src/test/com/aca/homework/week10/enums/list/PhoneModelTest.java
@@ -5,19 +5,19 @@ import org.junit.jupiter.api.Test;
 
 class PhoneModelTest {
     @Test
-    public void testNokia1100(){
+    public void testNokia1100() {
         PhoneModel nokia1100 = PhoneModel.NOKIA_1100;
         Assertions.assertEquals(0, nokia1100.cameras().size());
     }
 
     @Test
-    public void testNokia3110(){
+    public void testNokia3110() {
         PhoneModel nokia3110 = PhoneModel.NOKIA_3110;
         Assertions.assertEquals("back camera", nokia3110.cameras().get(0));
     }
 
     @Test
-    public void testGalaxy(){
+    public void testGalaxy() {
         PhoneModel galaxy = PhoneModel.GALAXY_S2;
         Assertions.assertEquals("back camera", galaxy.cameras().get(0));
         Assertions.assertEquals("front camera", galaxy.cameras().get(1));
