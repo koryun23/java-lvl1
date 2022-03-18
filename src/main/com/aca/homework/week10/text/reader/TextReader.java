a9c9ebaf902dec0c99b606dc0815e9266c273c1c
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-18 17:52:11 +0400

    solution of the 'text reader' problem

---
+++ src/main/com/aca/homework/week10/text/reader/TextReader.java
@@ -0,0 +1,32 @@
+package com.aca.homework.week10.text.reader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TextReader {
+
+
+    public static List<String> readLines() throws IOException {
+        List<String> list = new ArrayList<>();
+        Scanner scanner = new Scanner(getFile("doc-numbers.txt"));
+        while(scanner.hasNextLine()){
+            list.add(scanner.nextLine());
+        }
+        return list;
+    }
+
+    public static File getFile(final String filename){
+        final String mainPath = "src/main/com/aca/homework/week10/text/reader/";
+        return new File(String.format("./%s%s", mainPath, filename));
+    }
+
+    public static void print(List<String> list){
+        long index = 0;
+        for(String item : list){
+            System.out.println(++index + ") " + item);
+        }
+    }
+}
---
+++ src/main/com/aca/homework/week10/text/reader/TextReaderDemo.java
@@ -0,0 +1,10 @@
+package com.aca.homework.week10.text.reader;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TextReaderDemo {
+    public static void main(String[] args) throws IOException {
+        TextReader.print(TextReader.readLines());
+    }
+}
