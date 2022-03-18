986223680d8939a1f51bde982a010145adde2c17
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-18 18:56:35 +0400

    correcting the assertion logic and creating a testSubject in the BeforeEach method

--- src/test/com/aca/homework/week10/mapper/DefaultUserMapperTest.java
+++ src/test/com/aca/homework/week10/mapper/DefaultUserMapperTest.java
@@ -1,14 +1,18 @@
 package com.aca.homework.week10.mapper;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DefaultUserMapperTest {
+    private UserModel testSubject;
+    @BeforeEach
+    public void setUp(){
+        testSubject = new DefaultUserMapper().map(new User(1, "student", "Lord"));
+    }
     @Test
     public void testMap(){
-        User user = new User(1, "student", "Lord");
-        UserMapper mapper = new DefaultUserMapper();
-        Assertions.assertEquals(user.getFirstName(), mapper.map(user).getFirstName());
-        Assertions.assertEquals(user.getUsername(), mapper.map(user).getUsername());
+        Assertions.assertEquals("Lord", testSubject.getFirstName());
+        Assertions.assertEquals("student", testSubject.getUsername());
     }
 }
