24b412aa233c685f56d6c15be5c4b6cb25598d03
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-20 12:52:09 +0400

    creating a HashMap of Character and Operation buckets, changing the logics of methods of and isOperation

--- src/main/com/aca/homework/week9/calculator/Operation.java
+++ src/main/com/aca/homework/week9/calculator/Operation.java
@@ -1,12 +1,14 @@
 package com.aca.homework.week9.calculator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Operation {
     ADDITION {
         @Override
         public String performOperation(int num1, int num2) {
             return String.valueOf(num1 + num2);
         }
-
     },
     SUBTRACTION {
         @Override
@@ -27,25 +29,15 @@ public enum Operation {
         }
     };
 
-    public abstract String performOperation(int num1, int num2);
+    private static final Map<Character, Operation> map = Map.of('+', Operation.ADDITION, '-', Operation.SUBTRACTION, '*', Operation.MULTIPLICATION, '/', Operation.DIVISION);
 
     public static Operation of(char ch) {
-        switch (ch) {
-            case '+':
-                return Operation.ADDITION;
-            case '-':
-                return Operation.SUBTRACTION;
-            case '*':
-                return Operation.MULTIPLICATION;
-            case '/':
-                return Operation.DIVISION;
-            default:
-                return null;
-        }
+        return map.get(ch);
     }
+
     public static boolean isOperation(char ch) {
-        return ch == '/' || ch == '+' || ch == '-' || ch == '*';
+        return map.containsKey(ch);
     }
 
-
+    public abstract String performOperation(int num1, int num2);
 }
