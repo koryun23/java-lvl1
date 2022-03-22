70abf124ab0df3f2756034af97ea2fbe23fe5a1c
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-23 01:10:37 +0400

    JavaBeans pattern

---
+++ src/main/com/aca/effective/java/item2/javabeans/NutritionFactsDemo.java
@@ -0,0 +1,12 @@
+package com.aca.effective.java.item2.javabeans;
+
+public class NutritionFactsDemo {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts();
+        cocaCola.setServingSize(240);
+        cocaCola.setServings(8);
+        cocaCola.setSodium(35);
+        cocaCola.setCalories(100);
+        cocaCola.setCabrohydrate(27);
+    }
+}
