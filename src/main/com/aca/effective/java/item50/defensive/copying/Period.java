e1dfb65b062d821667e66aadfc407ae6ad6e6226
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-23 23:47:14 +0400

    items 2 3 and 50 from effective java with custom examples

--- src/main/com/aca/effective/java/item50/defensive/copying/Period.java
+++ src/main/com/aca/effective/java/item50/defensive/copying/Period.java
@@ -9,15 +9,15 @@ public class Period {
     public Period(Date start, Date end) {
         this.start = new Date(start.getTime());
         this.end = new Date(start.getTime());
-        if(this.start.compareTo(this.end) > 0)
+        if (this.start.compareTo(this.end) > 0)
             throw new IllegalArgumentException(this.start + " after " + this.end);
     }
 
-    public Date start(){
+    public Date start() {
         return new Date(start.getTime());
     }
 
-    public Date end(){
+    public Date end() {
         return new Date(end.getTime());
     }
 }
