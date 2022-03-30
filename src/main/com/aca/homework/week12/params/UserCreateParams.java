6401bfa4d7961078bea2c69ae712d61a492196ae
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-30 20:48:18 +0400

    non-finalized version of create-update-params problem

---
+++ src/main/com/aca/homework/week12/params/UserCreateParams.java
@@ -0,0 +1,31 @@
+package com.aca.homework.week12.params;
+
+public class UserCreateParams {
+    private String username;
+    private String firstName;
+    private int age;
+
+    public UserCreateParams(@NotNull String username, @NotNull String firstName, int age) {
+        setUsername(username);
+        setFirstName(firstName);
+        setAge(age);
+    }
+
+    public void setUsername(String username) {
+        if(username == null)
+            throw new IllegalArgumentException("Provided username is null.");
+        this.username = username;
+    }
+
+    public void setFirstName(String firstName){
+        if(firstName == null)
+            throw new IllegalArgumentException("Provided first name is null.");
+        this.firstName = firstName;
+    }
+
+    public void setAge(int age) {
+        if(age < 0)
+            throw new IllegalArgumentException("Provided age is negative.");
+        this.age = age;
+    }
+}
---
+++ src/main/com/aca/homework/week12/params/UserUpdateParams.java
@@ -0,0 +1,10 @@
+package com.aca.homework.week12.params;
+
+public class UserUpdateParams {
+
+    private String username;
+    private String firstName;
+    private int age;
+
+    public UserUpdateParams(){}
+}
