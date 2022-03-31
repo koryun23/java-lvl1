7f65b6beca892ec7f9a76a4711470852f581c8f5
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-04-01 00:14:47 +0400

    solution of the possible numbers count problem

--- src/main/com/aca/homework/week12/possible/numbers/count/PossibleNumbersCount.java
+++ src/main/com/aca/homework/week12/possible/numbers/count/PossibleNumbersCount.java
@@ -9,21 +9,19 @@ public class PossibleNumbersCount {
         System.out.println(getPossibleNumbersCount());
     }
 
-    public static int getPossibleNumbersCount(){
+    public static int getPossibleNumbersCount() {
         int count = 1;
         Set<Integer> numberSet = new HashSet<>(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
-        for(int i = 0; i < 4; i++) {
-            if(i == 2) {
+        Set<Integer> digitsWithNoOccurrenceLimitation = new HashSet<>(List.of(3));
+        for (int i = 1; i <= 4; i++) {
+            if (digitsWithNoOccurrenceLimitation.contains(i)) {
                 count *= 10; // no limitation for the third digit
-            } else if(i == 0) {
-                count *= (numberSet.size()-1); // omitting the zero
+            } else if (i == 1) { // the first digit
+                count *= (numberSet.size() - 1); // omitting the zero
                 numberSet.remove(0);
-            } else if(i == 1) {
-                count *= (numberSet.size());
-                numberSet.remove(1);
             } else {
                 count *= numberSet.size();
-                numberSet.remove(2);
+                numberSet.remove(i - 1);
             }
         }
         return count;
