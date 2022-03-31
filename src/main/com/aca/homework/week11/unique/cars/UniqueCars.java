7c083157a99a75b9e4e4011510490d7a3d32c732
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-03-31 11:44:29 +0400

    formatting the code, creating a nested static class implementing the Comparator interface, printing all cars instead of loading them in a string builder

--- src/main/com/aca/homework/week11/unique/cars/UniqueCars.java
+++ src/main/com/aca/homework/week11/unique/cars/UniqueCars.java
@@ -11,17 +11,34 @@ public class UniqueCars {
         this.supplier = supplier;
     }
 
+    private static void printAllCars(Set<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car);
+        }
+    }
+
+    private static int numberOfUniqueCars(Set<Car> cars) {
+        return cars.size();
+    }
+
+    private static boolean isEnd(String input) {
+        return "finish".equals(input);
+    }
+
+    private static String getCarBrandName(String[] params) {
+        return params[1];
+    }
+
+    private static String getCarModelName(String[] params) {
+        return params[2];
+    }
+
+    private static boolean invalidInput(String[] params) {
+        return params.length != 3;
+    }
+
     public Set<Car> askAndCalculate() {
-        Set<Car> cars = new TreeSet<>(new Comparator<Car>() {
-            @Override
-            public int compare(Car o1, Car o2) {
-                int result = o1.getBrand().compareTo(o2.getBrand());
-                if(result == 0){
-                    return o1.getModel().compareTo(o2.getModel());
-                }
-                return result;
-            }
-        });
+        Set<Car> cars = new TreeSet<>(new CarComparator());
         String input;
         while (true) {
             System.out.println("Please type your name, the brand, and model of your car(or type finish to terminate the program).");
@@ -42,34 +59,20 @@ public class UniqueCars {
     }
 
     public void printAllData(Set<Car> cars) {
-        System.out.println("Number of unique brand and models: " + numberOfUniqueCars(cars) + "\n" + allCarsToString(cars));
+        System.out.println("Number of unique brand and models: " + numberOfUniqueCars(cars));
+        printAllCars(cars);
     }
 
-    private static String allCarsToString(Set<Car> cars) {
-        StringBuilder sb = new StringBuilder();
-        for(Car car : cars) {
-            sb.append(car);
-            sb.append("\n");
+    private static class CarComparator implements Comparator<Car> {
+
+        @Override
+        public int compare(Car o1, Car o2) {
+            int result = o1.getBrand().compareTo(o2.getBrand());
+            if (result == 0) {
+                return o1.getModel().compareTo(o2.getModel());
+            }
+            return result;
         }
-        return sb.toString();
-    }
-    private static int numberOfUniqueCars(Set<Car> cars){
-        return cars.size();
-    }
-    private static boolean isEnd(String input) {
-        return "finish".equals(input);
-    }
-
-    private static String getCarBrandName(String[] params) {
-        return params[1];
-    }
-
-    private static String getCarModelName(String[] params) {
-        return params[2];
-    }
-
-    private static boolean invalidInput(String[] params) {
-        return params.length != 3;
     }
 
 }
