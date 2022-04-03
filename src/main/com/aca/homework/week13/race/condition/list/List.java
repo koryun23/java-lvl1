5bbb0fa148e4db9d4ef4dcd406a2fc32c3989717
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-04-03 22:43:10 +0400

    removing the solution from the main branch

--- src/main/com/aca/homework/week13/race/condition/list/List.java
+++
@@ -1,7 +0,0 @@
-package com.aca.homework.week13.race.condition.list;
-
-public interface List<E> {
-    void add(E item);
-
-    int size();
-}
--- src/main/com/aca/homework/week13/race/condition/list/Main.java
+++
@@ -1,9 +0,0 @@
-package com.aca.homework.week13.race.condition.list;
-
-public class Main {
-    public static void main(String[] args) {
-        RaceConditionGenerator raceConditionGenerator = new RaceConditionGenerator();
-        System.out.println(raceConditionGenerator.start());
-
-    }
-}
