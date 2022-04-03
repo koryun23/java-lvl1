181daf9c18efbf8224f4ce52001776ced90664f2
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-04-03 13:17:55 +0400

    The solution of the 'consumer test' problem

---
+++ src/main/com/aca/homework/week13/consumer/PrinterConsumer.java
@@ -0,0 +1,29 @@
+package com.aca.homework.week13.consumer;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class PrinterConsumer implements Consumer<String> {
+    private int timesAccessed = 0;
+    private List<String> argumentsTaken;
+
+    public PrinterConsumer(){
+        argumentsTaken = new LinkedList<>();
+    }
+
+    @Override
+    public void accept(String s) {
+        System.out.println(s);
+        timesAccessed++;
+        argumentsTaken.add(s);
+    }
+
+    public int getTimesAccessed() {
+        return timesAccessed;
+    }
+
+    public List<String> getArgumentsTaken(){
+        return argumentsTaken;
+    }
+}
