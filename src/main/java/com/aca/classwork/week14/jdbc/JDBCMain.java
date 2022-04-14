d3c7c2bcee784d1350e05810d03b6ad97723182c
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-04-14 23:18:50 +0400

    updating build.gradle and pushing 13.04 classwork

---
+++ src/main/java/com/aca/classwork/week14/jdbc/JDBCMain.java
@@ -0,0 +1,18 @@
+package com.aca.classwork.week14.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JDBCMain {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                "jdbc:postgresql://127.0.0.1:8080/aca", "postgres", null
+        );
+
+        connection.createStatement().execute("insert into users (name, age) values ('test', 18)");
+
+        ResultSet resultSet = connection.createStatement().executeQuery("select * from users");
+    }
+}
