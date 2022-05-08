010668b2a6444d7a17cb05b5320a7e3e532affad
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-05-08 15:09:42 +0400

    university facade problem

--- src/main/java/com/aca/classwork/week15/university/entity/User.java
+++ src/main/java/com/aca/classwork/week15/university/entity/User.java
@@ -1,6 +1,7 @@
 package com.aca.classwork.week15.university.entity;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity
@@ -10,6 +11,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDate date;
 
     @Column(name = "username", unique = true, nullable = false)
     private String username;
@@ -21,11 +25,13 @@ public class User {
     private String secondName;
 
     public User() {}
-    public User(String username, String firstName, String secondName) {
+    public User(String username, String firstName, String secondName, LocalDate createdAt) {
+        this.date = createdAt;
         this.username = username;
         this.firstName = firstName;
         this.secondName = secondName;
     }
+
 
     public String getUsername() {
         return username;
@@ -59,14 +65,12 @@ public class User {
         this.secondName = secondName;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", secondName='" + secondName + '\'' +
-                '}';
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 
     @Override
@@ -74,11 +78,22 @@ public class User {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(firstName, user.firstName) && Objects.equals(secondName, user.secondName);
+        return Objects.equals(id, user.id) && Objects.equals(date, user.date) && Objects.equals(username, user.username) && Objects.equals(firstName, user.firstName) && Objects.equals(secondName, user.secondName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, username, firstName, secondName);
+        return Objects.hash(id, date, username, firstName, secondName);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", date=" + date +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                '}';
     }
 }
