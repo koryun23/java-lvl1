166f0898dd6d9d1c7ce2416fd450259af7329889
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-05-17 20:12:37 +0400

    added an http client

--- src/main/java/com/aca/homework/week20/retrofit/cat/Client.java
+++ src/main/java/com/aca/homework/week20/retrofit/cat/Client.java
@@ -1,14 +1,17 @@
 package com.aca.homework.week20.retrofit.cat;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Client {
 
     public static Retrofit getClient(String url) {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
         return new Retrofit.Builder()
                 .baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
                 .build();
     }
 }
