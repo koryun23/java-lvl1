780887cd9957872cd9a2a3fc72b756814e6d01d6
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-05-17 21:25:01 +0400

    applying minor changes

--- src/main/java/com/aca/homework/week20/retrofit/cat/Main.java
+++ src/main/java/com/aca/homework/week20/retrofit/cat/Main.java
@@ -1,6 +1,7 @@
 package com.aca.homework.week20.retrofit.cat;
 
 import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 import java.io.IOException;
@@ -9,12 +10,16 @@ public class Main {
     public static void main(String[] args) {
         Retrofit retrofit = Client.getClient("https://catfact.ninja/");
         Service service = retrofit.create(Service.class);
-        FactDto factDto = null;
+        Response<FactDto> response;
         try {
-            factDto = service.getFactDto().execute().body();
+            response = service.getFactDto().execute();
         } catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException(e);
         }
+
+        FactDto factDto = response.body();
         System.out.println(factDto);
+
     }
 }
